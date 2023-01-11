b3f2d889e138dc38d8ef4c66566e377fa179d202
Mateus do Vale Alexandre <mateusvalexandre@gmail.com>
2023-01-11 07:20:12 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from script_functions import *
+
+linha = "600"
+onibus_ordem = "C47588"
+address_img_files = "/Users/mateus/Personal/raspberry/img_files"
+address_in_circle = "/Users/mateus/Personal/raspberry/in_circle"
+
+# marketing = get_all_marketing_from_db()
+new_marketing_in_internal_db() #v
---
+++ script_functions.py
@@ -0,0 +1,167 @@
+import requests
+import json
+import math
+import os
+import time
+
+linha = "600"
+onibus_ordem = "C47588"
+address_img_files = "/Users/mateus/Personal/raspberry/img_files"
+address_in_circle = "/Users/mateus/Personal/raspberry/in_circle"
+
+########################################################################################################
+
+## GPS onibus ##
+def get_bus_gps(linha_onibus = 'all', ordem = "none"):
+    url = f'https://db-postgress-tcc.herokuapp.com/onibus/{linha_onibus}'
+    x = requests.get(url)
+    results = json.loads(x.text)
+    if ordem != "none":
+        for entry in results:
+            if entry['ordem'] == ordem:
+                return entry 
+    # print(results)
+    return results
+
+def are_points_in_the_circle(checkPoint, centerPoint, m): #(circle, bus, radius)
+    km = m/1000
+    ky = 40000 / 360
+    kx = math.cos(math.pi * centerPoint['lat'] / 180.0) * ky
+    dx = abs(centerPoint['lng'] - checkPoint['lng']) * kx
+    dy = abs(centerPoint['lat'] - checkPoint['lat']) * ky
+    return math.sqrt(dx * dx + dy * dy) <= km
+
+## GPS onibus ##
+
+########################################################################################################
+
+## gerenciamento do DB ##
+
+def get_all_marketing_from_db():
+    url = 'https://db-postgress-tcc.herokuapp.com/marketing'
+    x = requests.get(url)
+    results = json.loads(x.text)
+    marketing = []
+    for result in results:
+        if result['bus_line'] == linha:
+            marketing.append(result)
+    return marketing
+
+def get_marketing_from_db(marketing_id):
+    url = f'https://db-postgress-tcc.herokuapp.com/marketing/{marketing_id}'
+    x = requests.get(url)
+    return json.loads(x.text)
+
+def download_image(image_url, name = 'image'):
+    response = requests.get(image_url)
+    file = f'./img_files/{name}.jpeg'
+    open(file, "wb").write(response.content)
+
+## gerenciamento do DB ##
+
+########################################################################################################
+
+## gerenciamento do internal_db ##
+
+def new_marketing_in_internal_db():
+    marketings = get_all_marketing_from_db(linha)
+    internal_db = read_internal_db()
+    for marketing in marketings:
+        new_marketing = True
+        for entry in internal_db:
+            if entry["user_id"] == marketing["user_id"] and entry["marketing_id"] == marketing["id"]:
+                new_marketing = False
+                break
+        if new_marketing == True:
+            name = f'{marketing["user_id"]}-{marketing["id"]}'
+            download_image(marketing["image_url"], name)
+            internal_db.append({
+                "user_id": marketing["user_id"],
+                "marketing_id": marketing["id"],
+                "code": 0 
+            })
+    json_data = json.dumps(internal_db)
+    write_internal_db(json_data)
+
+def write_internal_db(internal_db):
+    file = open("internal_db.json", "w")
+    file.write(internal_db)
+    file.close()
+
+def read_internal_db():
+    file = open("internal_db.json", "r")
+    internal_db = file.read()
+    internal_db = json.loads(internal_db)
+    return internal_db
+
+## gerenciamento do internal_db ##
+
+########################################################################################################
+
+## reading in img_files folder ##
+
+def read_img_files_folder():
+    files = os.listdir(address_img_files)
+    marketing_list = get_all_marketing_from_db()
+    info_files = []
+
+    for file in files:
+        user_id = file.split('.')[0].split('-')[0]
+        marketing_id = file.split('.')[0].split('-')[1]
+        marketing = {}
+        marketing = [x for x in marketing_list if str(x['id']) == marketing_id][0]
+        info_files.append({
+            'name': file,
+            'user_id': user_id,
+            'marketing_id': marketing_id,
+            'radius': marketing['radius'],
+            'lat': marketing['lat'],
+            'lng': marketing['lng']
+        })
+    return info_files
+
+## reading in img_files folder ##
+
+########################################################################################################
+
+## managing in in_circle folder ##
+
+def manage_in_circle_folder():
+    bus_location = get_bus_gps(linha, onibus_ordem)
+    files = read_img_files_folder()
+    for file in files:
+        # print(bus_location)
+        destination = f'{address_in_circle}/{file["name"]}'
+        bus = {'lat': float(bus_location[0]['latitude']), 'lng': float(bus_location[0]['longitude'])}
+        inside_circle = are_points_in_the_circle(file, bus, file['radius'])
+        if (inside_circle):
+            copy_code = f'cp {address_img_files}/{file["name"]} {destination}'
+            os.system(copy_code)
+        elif os.path.exists(destination):
+            os.remove(destination)
+
+## managing in in_circle folder ##
+
+########################################################################################################
+
+## desktop background ##
+
+def desktop_background(files):
+    files = "" # transform to array
+    status = "" # read the file status
+    folder_in_circle = "" # folder that contains the image files that will be prompt in the screen
+    img_default_location = ""
+    if len(files) == 0:
+        os.system(f'pcmanfm --set-wallpaper {img_default_location}')
+    else:
+        if status == "running":
+            time.sleep(2)
+        file = files.pop()
+        os.system(f'pcmanfm --set-wallpaper {folder_in_circle}/{file}')
+        new_file = [file] + files
+        # escreve o array new_file no arquivo files
+
+
+
+# 'pcmanfm --set-wallpaper /home/pi/Downloads/foto1.jpg'
+## desktop background ##
